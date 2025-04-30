1139ae4b0b1319f2c9c807d50c840a08c9701d98
Pedro Paulo <49731265+pedropaulowski@users.noreply.github.com>
2025-04-30 19:56:32 -0300

    Add files via upload

---
+++ socc/src/main/java/com/ufg/socc/SoccApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.socc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccApplication.class, args);
+	}
+
+}
