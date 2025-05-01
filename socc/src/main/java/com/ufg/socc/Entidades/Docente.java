56148d448e116f7cc984039336265fde3061c739
Vitor Soares <moreirasoaresvitor@gmail.com>
2025-04-30 21:15:02 -0300

    feat: docente entity

---
+++ socc/src/main/java/com/ufg/socc/Entidades/Docente.java
@@ -0,0 +1,16 @@
+package com.ufg.socc.Entidades;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Table(name = "docentes")
+@Entity
+public class Docente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String usuario;
+    private String email;
+    private Date ingresso;
+}
--- socc/src/main/java/com/ufg/socc/Entidades/NucleoConhecimento.java
+++ socc/src/main/java/com/ufg/socc/Entidades/NucleoConhecimento.java
@@ -4,7 +4,7 @@ package com.ufg.socc.Entidades;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="nucleo_conhecimento")
+@Table(name="nucleos_conhecimento")
 public class NucleoConhecimento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
