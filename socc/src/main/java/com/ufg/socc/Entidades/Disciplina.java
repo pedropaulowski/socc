c3ef01deac428c037c0889f8b1fed088e4c14113
salvadorludovico <49698940+salvadorludovico@users.noreply.github.com>
2025-04-30 21:10:22 -0300

    feat:  added disciplina entity and lombok dependency

--- socc/src/main/java/com/ufg/socc/Entidades/Disciplina.java
+++ socc/src/main/java/com/ufg/socc/Entidades/Disciplina.java
@@ -1,4 +1,21 @@
 package com.ufg.socc.Entidades;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Table(name= "disciplina")
+@Entity
 public class Disciplina {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String usuario;
+    private Integer nucleo_id;
+    private Integer matriz;
+    private Integer ch_teorica;
+    private Integer ch_pratica;
+    private Integer ch_total;
 }
