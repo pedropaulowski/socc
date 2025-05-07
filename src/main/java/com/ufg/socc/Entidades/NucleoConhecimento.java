f0422eff57a0e509cfcd8bc01e4434b6f60a97e2
Vitor Soares <moreirasoaresvitor@gmail.com>
2025-05-07 19:10:54 -0300

    fix: folders

--- src/main/java/com/ufg/socc/Entidades/NucleoConhecimento.java
+++ src/main/java/com/ufg/socc/Entidades/NucleoConhecimento.java
@@ -4,7 +4,7 @@ package com.ufg.socc.Entidades;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="NucleoConhecimento")
+@Table(name="nucleos_conhecimento")
 public class NucleoConhecimento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
