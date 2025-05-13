30ce2b887a18c68a597f50d137932f9d4eb973f3
pedropaulwoski <dos_pedro@discente.ufg.br>
2025-05-13 19:07:01 -0300

    Repository NucleoConhecimento

---
+++ src/main/java/com/ufg/socc/Repositorio/NucleoConhecimentoRepository.java
@@ -0,0 +1,7 @@
+package com.ufg.socc.Repositorio;
+
+import com.ufg.socc.Entidades.NucleoConhecimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NucleoConhecimentoRepository extends JpaRepository<NucleoConhecimento, Long> {
+}
