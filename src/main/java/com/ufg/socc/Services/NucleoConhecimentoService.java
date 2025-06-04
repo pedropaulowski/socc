c19d2683a6384e27eb87c430a53d9d7e05756485
pedropaulwoski <dos_pedro@discente.ufg.br>
2025-06-03 22:58:31 -0300

    feat buscar todos núcleos

---
+++ src/main/java/com/ufg/socc/Services/NucleoConhecimentoService.java
@@ -0,0 +1,11 @@
+package com.ufg.socc.Services;
+
+import com.ufg.socc.Entidades.NucleoConhecimentoEntidade;
+
+import java.util.List;
+
+public interface NucleoConhecimentoService {
+    List<NucleoConhecimentoEntidade> buscarTodosNucleos();
+
+
+}
