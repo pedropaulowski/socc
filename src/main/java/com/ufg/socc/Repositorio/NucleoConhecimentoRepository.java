7083d22ff94d126aa0579682cae419b3724f1ed3
pedropaulwoski <dos_pedro@discente.ufg.br>
2025-06-03 22:57:51 -0300

    fix Repository nucleo conhecimento

--- src/main/java/com/ufg/socc/Repositorio/NucleoConhecimentoRepository.java
+++ src/main/java/com/ufg/socc/Repositorio/NucleoConhecimentoRepository.java
@@ -1,7 +1,16 @@
 package com.ufg.socc.Repositorio;
 
-import com.ufg.socc.Entidades.NucleoConhecimento;
+import com.ufg.socc.Entidades.NucleoConhecimentoEntidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface NucleoConhecimentoRepository extends JpaRepository<NucleoConhecimento, Long> {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NucleoConhecimentoRepository extends JpaRepository<NucleoConhecimentoEntidade, Long> {
+
+    Optional<NucleoConhecimentoEntidade> findById(Long id);
+
+    List<NucleoConhecimentoEntidade> findAll();
 }
